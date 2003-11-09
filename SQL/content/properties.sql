6eaaa1fed5d95e4574c038b3dac6f18a644862c8
Sergey <sergey@example.com>
2003-11-09 00:09:12 +0000

    System properties engine

---
+++ SQL/content/properties.sql
@@ -0,0 +1,2 @@
+INSERT INTO properties (name, value, format, type, maker, note) VALUES
+    ('last_birthdays_check', '17.07.1980', 'dd.MM.yyyy', 'java.util.Date', 'su.sergey.contacts.properties.impl.DatePropertyMaker', 'Дата последней проверки информации о ближайших днях рождения');
