2628bc2046b64d6731256c82a0cbdefc23adc6d7
Sergey <sergey@example.com>
2004-03-04 17:44:12 +0000

    Additional phone types

--- SQL/content/phone_types.sql
+++ SQL/content/phone_types.sql
@@ -1,8 +1,9 @@
-INSERT INTO phone_types (name) VALUES ('Домашний');
-INSERT INTO phone_types (name) VALUES ('Мобильный');
-INSERT INTO phone_types (name) VALUES ('Рабочий');
-INSERT INTO phone_types (name) VALUES ('Факс');
-INSERT INTO phone_types (name) VALUES ('Внутренний рабочий');
-INSERT INTO phone_types (name) VALUES ('Пейджер');
-INSERT INTO phone_types (name) VALUES ('Искра');
+INSERT INTO phone_types (id, name) VALUES (  1, 'Домашний');
+INSERT INTO phone_types (id, name) VALUES (  2, 'Мобильный');
+INSERT INTO phone_types (id, name) VALUES (  3, 'Рабочий');
+INSERT INTO phone_types (id, name) VALUES (  4, 'Факс');
+INSERT INTO phone_types (id, name) VALUES (  5, 'Внутренний рабочий');
+INSERT INTO phone_types (id, name) VALUES (  6, 'Пейджер');
+INSERT INTO phone_types (id, name) VALUES (  7, 'Искра');
+INSERT INTO phone_types (id, name) VALUES ( 99, 'Старый');
 INSERT INTO phone_types (id, name) VALUES (100, 'Другой');
--- SQL/tables/phone_types.sql
+++ SQL/tables/phone_types.sql
@@ -1,18 +1,9 @@
 DROP TABLE phone_types;
-DROP SEQUENCE phone_types_id_seq;
 
 BEGIN;
 
-CREATE SEQUENCE phone_types_id_seq
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    CYCLE;
-
 CREATE TABLE phone_types (
-    id int4 NOT NULL
-            DEFAULT nextval('phone_types_id_seq'::text),
+    id int4 NOT NULL,
     name text NOT NULL
               CHECK (name != '')
               UNIQUE,
@@ -22,14 +13,11 @@ CREATE TABLE phone_types (
 COMMENT ON TABLE phone_types IS 'Типы телефонов';
 COMMENT ON COLUMN phone_types.id IS 'Идентификатор типа телефона';
 COMMENT ON COLUMN phone_types.name IS 'Тип телефона';
-COMMENT ON SEQUENCE phone_types_id_seq IS 'Генератор идентификаторов типов телефонов';
 
-REVOKE ALL ON phone_types, phone_types_id_seq FROM PUBLIC;
-REVOKE ALL ON phone_types, phone_types_id_seq FROM j2eeagent;
-REVOKE ALL ON phone_types, phone_types_id_seq FROM apacheagent;
+REVOKE ALL ON phone_types FROM PUBLIC;
+REVOKE ALL ON phone_types FROM j2eeagent;
+REVOKE ALL ON phone_types FROM apacheagent;
 GRANT SELECT, INSERT, UPDATE, DELETE ON phone_types TO apacheagent;
 GRANT SELECT, INSERT, UPDATE, DELETE ON phone_types TO j2eeagent;
-GRANT SELECT, UPDATE ON phone_types_id_seq TO apacheagent;
-GRANT SELECT, UPDATE ON phone_types_id_seq TO j2eeagent;
 
 COMMIT;
