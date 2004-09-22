5eb196ff0823fc0d7a4713e4edc4609f0b016586
Sergey <sergey@example.com>
2004-09-22 16:50:31 +0000

    Fix inconsistency in conversion from numeric values into strings

--- SQL/system/functions.sql
+++ SQL/system/functions.sql
@@ -51,11 +51,17 @@ CREATE FUNCTION set_update_time()
 	END;
     '
     LANGUAGE 'plpgsql';
+
+CREATE FUNCTION text(numeric(30, 6))
+    RETURNS text
+    AS 'return $_[0];'
+    LANGUAGE 'perl';
     
 COMMENT ON FUNCTION unique_basic(int4, int4, boolean) IS 'Возвращает значение, одинаковое для двух основных объектов одного человека';
 COMMENT ON FUNCTION text(date) IS 'Преобразование даты в текст';
 COMMENT ON FUNCTION phone_norm(text) IS 'Нормализация номеров телефонов в некоторых форматах';
 COMMENT ON FUNCTION set_insert_time() IS 'Установка в триггере времени заведения записи';
 COMMENT ON FUNCTION set_update_time() IS 'Установка в триггере времени последнего обновления записи';
+COMMENT ON FUNCTION text(numeric(30, 6)) IS 'Преобразование в текст типа, используемого для представления денежных сумм';
 
 COMMIT;
