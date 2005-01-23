5dc233fce146edee8d3cbfa515394727f4076220
Sergey <sergey@example.com>
2005-01-23 00:01:50 +0000

    Immutable pattern applied

--- ContactsEJB/ejbModule/su/sergey/contacts/person/valueobjects/Person2.java
+++ ContactsEJB/ejbModule/su/sergey/contacts/person/valueobjects/Person2.java
@@ -3,8 +3,33 @@ package su.sergey.contacts.person.valueobjects;
 import java.io.Serializable;
 
 import su.sergey.contacts.dto.PersonHandle;
+import su.sergey.contacts.person.valueobjects.PersonAttributes;
 
-public interface Person2 extends Serializable {
-	PersonHandle getHandle();
-	PersonAttributes getAttributes();
+public class Person2 implements Serializable {
+	private PersonHandle handle;
+	private PersonAttributes attributes;
+
+	/**
+	 * Constructor for Person2
+	 */
+	public Person2(PersonHandle handle, PersonAttributes attributes) {
+		this.handle = handle;
+		this.attributes = attributes;
+	}
+
+	/**
+	 * Gets the handle
+	 * @return Returns a PersonHandle
+	 */
+	public PersonHandle getHandle() {
+		return handle;
+	}
+	
+	/**
+	 * Gets the attributes
+	 * @return Returns a Person
+	 */
+	public PersonAttributes getAttributes() {
+		return attributes;
+	}
 }
--- ContactsEJB/ejbModule/su/sergey/contacts/supply/valueobjects/Supply2.java
+++ ContactsEJB/ejbModule/su/sergey/contacts/supply/valueobjects/Supply2.java
@@ -3,8 +3,33 @@ package su.sergey.contacts.supply.valueobjects;
 import java.io.Serializable;
 
 import su.sergey.contacts.dto.SupplyHandle;
+import su.sergey.contacts.supply.valueobjects.SupplyAttributes;
 
-public interface Supply2 extends Serializable {
-	SupplyHandle getHandle();
-	SupplyAttributes getAttributes();
+public class Supply2 implements Serializable {
+	private SupplyHandle handle;
+	private SupplyAttributes attributes;
+
+	/**
+	 * Constructor for Supply2
+	 */
+	public Supply2(SupplyHandle handle, SupplyAttributes attributes) {
+		this.handle = handle;
+		this.attributes = attributes;
+	}
+
+	/**
+	 * Gets the handle
+	 * @return Returns a SupplyHandle
+	 */
+	public SupplyHandle getHandle() {
+		return handle;
+	}
+	
+	/**
+	 * Gets the attributes
+	 * @return Returns a Supply
+	 */
+	public SupplyAttributes getAttributes() {
+		return attributes;
+	}
 }
